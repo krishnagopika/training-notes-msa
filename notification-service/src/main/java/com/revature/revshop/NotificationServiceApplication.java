58719ab6f0d35a1b527012f0427ccaf82ccc63fe
krishnagopika <103101208+krishnagopika@users.noreply.github.com>
2024-09-30 12:45:09 +0530

    Update NotificationServiceApplication.java

--- notification-service/src/main/java/com/revature/revshop/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/revature/revshop/NotificationServiceApplication.java
@@ -3,46 +3,65 @@ package com.revature.revshop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import com.revature.revshop.event.OrderEvent;
+import com.revature.revshop.config.OrderEventDeserializer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
 
-	@Autowired
-	private JavaMailSender mailSender;
-	
-	
-		
-	
-	public void sendEmail(String to, String subject, String body) {
-		
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setFrom("revshop@demomailtrap.com");
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(body);
-		
-		mailSender.send(message);
-	}
-	
-	
-	
-	@KafkaListener(topics="notificationTopic")
-	public void getOrderEvent(OrderEvent orderEvent) {
-		
-		System.out.println("recieved event" +orderEvent.toString());
-		
-		sendEmail("krishnagopika1701@gmail.com", "Rev Shop: Order placed", orderEvent.getOrderNumber());
-		
-		
-	}
+    @Autowired
+    private JavaMailSender mailSender;
 
+    @Bean
+    public ConsumerFactory<String, OrderEvent> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "notificationId");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, OrderEventDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, OrderEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, OrderEvent> factory = 
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("revshop@demomailtrap.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        
+        mailSender.send(message);
+    }
+
+    @KafkaListener(topics = "notificationTopic", groupId = "notificationId")
+    public void getOrderEvent(OrderEvent orderEvent) {
+        System.out.println("Received event: " + orderEvent.toString());
+        
+        sendEmail("krishnagopika1701@gmail.com", "Rev Shop: Order placed", orderEvent.getOrderNumber());
+    }
 }
